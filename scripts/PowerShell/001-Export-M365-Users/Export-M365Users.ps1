f79718441032e10998d14a222d629b1bd7bb05ce
Niranjan Babu <niranjanpamarthi5@gmail.com>
2026-08-06 18:03:32 -0500

    Initialize Export-M365Users project

---
+++ scripts/PowerShell/001-Export-M365-Users/Export-M365Users.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+Exports Microsoft 365 users to a CSV file.
+
+.DESCRIPTION
+This script will connect to Microsoft Graph, retrieve all Microsoft 365 users,
+and export them to a CSV report.
+
+.AUTHOR
+Niranjan Babu
+
+.VERSION
+0.1.0
+#>
+
+Write-Host "Export-M365Users project initialized."
