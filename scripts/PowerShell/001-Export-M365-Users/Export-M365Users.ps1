6a9365bf872c8725a5b2827e4009777899c18a09
Niranjan Babu <niranjanpamarthi5@gmail.com>
2026-08-11 08:09:25 -0500

    Improve M365 user export script

--- scripts/PowerShell/001-Export-M365-Users/Export-M365Users.ps1
+++ scripts/PowerShell/001-Export-M365-Users/Export-M365Users.ps1
@@ -2,15 +2,29 @@
 .SYNOPSIS
 Exports Microsoft 365 users to a CSV file.
 
-.DESCRIPTION
-This script will connect to Microsoft Graph, retrieve all Microsoft 365 users,
-and export them to a CSV report.
-
 .AUTHOR
 Niranjan Babu
 
 .VERSION
-0.1.0
+1.2.0
 #>
 
-Write-Host "Export-M365Users project initialized."
+param(
+    [string]$OutputPath = ".\Users.csv"
+)
+
+$OutputFolder = Split-Path -Path $OutputPath -Parent
+
+if ($OutputFolder -and -not (Test-Path -Path $OutputFolder)) {
+    New-Item -ItemType Directory -Path $OutputFolder -Force | Out-Null
+    Write-Host "Created folder: $OutputFolder"
+}
+
+Connect-MgGraph -Scopes "User.Read.All" -NoWelcome
+
+$Users = Get-MgUser -All |
+    Select-Object DisplayName, UserPrincipalName, Id
+
+$Users | Export-Csv -Path $OutputPath -NoTypeInformation
+
+Write-Host "Export completed: $OutputPath"
